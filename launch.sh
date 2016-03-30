17842f7d1358b6576489b3e2968e83792c7e6040
Damien Garaud <damien.garaud@gmail.com>
2016-03-31 01:04:45 +0200

    fix a typo in 'launch.sh'

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # need reveal-md https://github.com/webpro/reveal-md
-reveal-md slides.md --highlightTheme monokai_sublime --theme my_black
+reveal-md slides.md --highlightTheme monokai-sublime --theme my_black
 
